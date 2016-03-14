d552fd0bab547241a4223020f208cb288a36a29c
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-03-14 17:00:50 -0600

    Added new favicons. Removed not_found_image from properties and used default.

--- uCount/src/org/BinghamTSA/uCount/web/ImageServlet.java
+++ uCount/src/org/BinghamTSA/uCount/web/ImageServlet.java
@@ -18,36 +18,36 @@ import org.BinghamTSA.uCount.core.utilities.PropertiesHelper;
 @WebServlet("/images/*")
 public class ImageServlet extends HttpServlet {
 
-	private File imageNotFoundFile;
+  private File imageNotFoundFile;
 
-	@Override
-	public void init() {
-		String imageNotFoundPath = PropertiesHelper.getApplicationProperty("not_found_image");
-		if (imageNotFoundPath != null) {
-			try {
-				imageNotFoundFile = new File(getServletContext().getResource(imageNotFoundPath).toURI());
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+  @Override
+  public void init() {
+    String imageNotFoundPath = "/resources/images/notFound.png";
+    if (imageNotFoundPath != null) {
+      try {
+        imageNotFoundFile = new File(getServletContext().getResource(imageNotFoundPath).toURI());
+      } catch (MalformedURLException e) {
+        e.printStackTrace();
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String filename = request.getPathInfo().substring(1);
-		File file = new File(FileUploadUtilities.IMAGE_UPLOADS_DIR, filename);
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String filename = request.getPathInfo().substring(1);
+    File file = new File(FileUploadUtilities.IMAGE_UPLOADS_DIR, filename);
 
-		if (!file.exists() || filename.isEmpty() || filename.equals("/"))
-			file = imageNotFoundFile;
+    if (!file.exists() || filename.isEmpty() || filename.equals("/"))
+      file = imageNotFoundFile;
 
-		if (file != null) {
-			response.setHeader("Content-Type", getServletContext().getMimeType(filename));
-			response.setHeader("Content-Length", String.valueOf(file.length()));
-			response.setHeader("Content-Disposition", "inline; filename=\"" + filename + "\"");
-			Files.copy(file.toPath(), response.getOutputStream());
-		}
-	}
+    if (file != null) {
+      response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+      response.setHeader("Content-Length", String.valueOf(file.length()));
+      response.setHeader("Content-Disposition", "inline; filename=\"" + filename + "\"");
+      Files.copy(file.toPath(), response.getOutputStream());
+    }
+  }
 
 }
